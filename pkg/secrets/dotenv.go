f020118f8fb1671e8125dea6711359de7662b95f
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-05-26 16:10:45 +0200

    load env

---
+++ pkg/secrets/dotenv.go
@@ -0,0 +1,30 @@
+package secrets
+
+import (
+	"log"
+	"os"
+
+	"github.com/dotenv-org/godotenvvault"
+)
+
+func LoadDotEnv() {
+	err := godotenvvault.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return
+}
+
+func GetEnv(v string) string {
+	LoadDotEnv()
+
+	val, ok := os.LookupEnv(v)
+
+	if !ok {
+		log.Fatal("Missing env variable: ", v)
+	}
+
+	return val
+}
